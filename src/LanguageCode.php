33ce7f799a001c9b51da45c4bb58fadbbef9c258
Josantonius <info@josantonius.com>
2017-03-18 05:20:30 +0100

    The repository was updated to version 1.1.1

--- src/LanguageCode.php
+++ src/LanguageCode.php
@@ -2,15 +2,11 @@
 /**
  * List of 217 language codes: ISO 639-1.
  * 
- * @category   JST
- * @package    LanguageCode
- * @subpackage LanguageCode
- * @author     Josantonius - info@josantonius.com
- * @copyright  Copyright (c) 2017 JST PHP Framework
+ * @author     Josantonius - hello@josantonius.com
+ * @copyright  Copyright (c) 2016 - 2017
  * @license    https://opensource.org/licenses/MIT - The MIT License (MIT)
- * @version    1.1.0
  * @link       https://github.com/Josantonius/PHP-LanguageCode
- * @since      File available since 1.0.0 - Update: 2017-01-30
+ * @since      1.0.0
  */
 
 namespace Josantonius\LanguageCode;
